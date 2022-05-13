13e31b22bb66c6185bf90e6a7c803a2dce39daef
Maxime <maximedonz39@gmail.com>
2022-05-13 08:40:18 +0200

    Changement du fond actuel pour un fond à la bonne taille qui ressemblera plus au résultat final

--- Game.py
+++ Game.py
@@ -6,13 +6,15 @@ pygame.init()
 fenetre = pygame.display.set_mode((600, 600))
 
 #écran d'accueil
-fond = pygame.image.load('plateau_python_party.png')
+fond = pygame.image.load('écran_accueil_ebauche.png')
 fenetre.blit(fond, (0, 0))
+
+
 
 #Actualise l'écran
 pygame.display.flip()
 
-#Pour fermer la fenêtre de jeu
+# détection d'évènements en jeu (cliquesouris/clavier, fermer la fenêtre,...) et réaction en fonction
 continuer = 1
 while continuer:
     for event in pygame.event.get():
